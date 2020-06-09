bd462d9b1fd907ed10e59e6b39b816276fa9bda1
Martin Kim <martinjwkim@gmail.com>
2020-06-09 00:32:43 -0700

    removed test msg

---
+++ frontend/src/NewGame.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import GameForm from './GameForm'
+import RoundForm from './RoundForm'
+import FinalScores from './FinalScores'
+import './NewGame.css'
+
+function NewGame() {
+
+  const [round, setRound] = useState(0);
+  const [players, setPlayers] = useState({});
+  const [scores, setScores] = useState({});
+  const [multiplier, setMultiplier] = useState(0.1)
+  const [finalRound, setFinalRound] = useState(10)
+  const [endGame, setEndGame] = useState(false)
+
+  return (
+    <div className='NewGame'>
+      {round === 0 &&
+        <GameForm
+          setRound={setRound}
+          setPlayers={setPlayers} />}
+      {round > 0 && round <= finalRound &&
+        <RoundForm
+          endGame={endGame}
+          setRound={setRound}
+          round={round}
+          players={players}
+          scores={scores}
+          setScores={setScores}
+          finalRound={finalRound}
+          multiplier={multiplier} />}
+      {round > finalRound &&
+        <FinalScores
+          players={players}
+          scores={scores}
+          setScores={setScores}
+          round={round}
+          multiplier={multiplier}
+          finalRound={finalRound}
+          setFinalRound={setFinalRound}
+          setEndGame={setEndGame}
+          endGame={endGame} />}
+    </div>
+  );
+}
+
+export default NewGame;
