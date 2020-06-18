815d639d421c49af320ded6dd412c008daff01b6
Martin Kim <martinjwkim@gmail.com>
2020-06-17 23:16:55 -0700

    wip rootreducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,8 @@
 const INITIAL_STATE =
 {
   round: 0,
-  players: {
-    player1: "Player 1",
-    player2: "Player 2",
-    player3: "Player 3",
-    player4: "Player 4"
-  },
+  players: {},
+  scores: {},
   endGame: false,
   multiplier: 0.1,
   finalRound: 10,
@@ -14,8 +10,8 @@ const INITIAL_STATE =
 
 function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case "INCREMENT":
-      return { ...state, count: state.count + 1 };
+    case "NEXT_ROUND":
+      return { ...state, round: state.round + 1 };
 
     case "DECREMENT":
       return { ...state, count: state.count - 1 };
