3f4efca60182af345ae553c1a41913d19c5cad2a
Martin Kim <martinjwkim@gmail.com>
2020-06-30 19:25:20 -0700

    private routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Scores from './Scores'
 import GraphContainer from './GraphContainer'
 import NotFound from './NotFound'
 import Login from './Login'
+import PrivateRoute from './PrivateRoute'
 
 
 function Routes({setToken}) {
@@ -14,9 +15,9 @@ function Routes({setToken}) {
       <Switch>
         <Route exact path="/"></Route>
         <Route exact path="/login"><Login setToken={setToken}/></Route>
-        <Route exact path="/newgame"><NewGame /></Route>
-        <Route exact path="/scores"><Scores /></Route>
-        <Route exact path="/graph"><GraphContainer /></Route>
+        <PrivateRoute exact path="/newgame"><NewGame /></PrivateRoute>
+        <PrivateRoute exact path="/scores"><Scores /></PrivateRoute>
+        <PrivateRoute exact path="/graph"><GraphContainer /></PrivateRoute>
         <Route><NotFound /></Route>
       </Switch>
     </div>
